c5e409c9c7d7524f01c950b847e81961d2245a44
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-08-16 16:40:57 -0700

    moved trivia questions to javascript. creating an array of variables of trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,34 @@
+$(document).ready(function() {
 //providing a function for the count down to begin starting at 10 seconds.
 //created function to run the time and countdown, giving alert to time's up, and resetting the clock
-$("#display").html("Time Remaining: 10:00");
+$("#display-time").html("Time Remaining: 10");
+})
+var trivia = {
+  time: 10,
+  timerId: '',
 
-var time = 10;
-var intervalId;
+  questions: {
+    q1: "Where is Loki's sceptre ultimately located?",
+    q2: "Which of the following Avengers from the comics is not in the movie?",
+    q3: "Who does Captain America call Earth's best defender?",
+  },
+}
+
 
 function run() {
-  clearInterval(intervalId);
+  if (!clockRunning){
   intervalId = setInterval(decrement, 1000);
+  clockRunning = true;
+}
 }
 
 function decrement(){
 
   time--;
 
-  $('display-time').html('<h2>' + time + '</h2>')
+  $('#display-time').html('<h2>' + time + '</h2>')
   
-  if (time ===0) {
+  if (time === 0) {
 
     stop();
 
@@ -30,3 +42,19 @@ function stop(){
 }
 
 run();
+
+// <!-- Creating trivia questions-->
+// <h3>Where is Loki's sceptre ultimately located?
+//   <p>Sokovia</p> <!--Answer-->
+//   <p>Thanos</p>
+//   <p>America</p>
+//   <h3>Which of the following Avengers from the comics is not in the movie?
+//       <p>Captain America</p>
+//       <p>The Wasp</p> <!--Answer-->
+//       <p>Hawkeye</p>
+// </h3>
+// <h3>Who does Captain America call "Earth's best defender?"
+//     <p>Spider Man</p>
+//     <p>Hulk</p> 
+//     <p>Iron Man</p><!--Answer-->
+// </h3>
