ad4b37eed6ce7aeadd48dc389aa959d94c8c15f3
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-08-16 16:21:46 -0700

    created trivia questions and time of the page is at 10 seconds to answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,32 @@
-$("#display").html("Time Remaining: 00:00");
+//providing a function for the count down to begin starting at 10 seconds.
+//created function to run the time and countdown, giving alert to time's up, and resetting the clock
+$("#display").html("Time Remaining: 10:00");
+
+var time = 10;
+var intervalId;
+
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement(){
+
+  time--;
+
+  $('display-time').html('<h2>' + time + '</h2>')
+  
+  if (time ===0) {
+
+    stop();
+
+    alert("Time's Up!")
+  }
+}
+
+function stop(){
+  
+  clearInterval(intervalId);
+}
+
+run();
